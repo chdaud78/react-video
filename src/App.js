10de0bf76f7f92b5ad91a009f36ee121bc23d049
chdaud78 <qkrri56@naver.com>
2022-01-06 16:55:28 +0900

    [ENH] React Hook2
    - useEffect
    - useTitle

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import React, {useState} from "react";
-
-const content = [
-    {
-        tab: "Section 1",
-        content: "I'm the content of the Section 1"
-    },
-    {
-        tab: "Section 2",
-        content: "I'm the content of the Section 2"
-    }
-]
-
-const useTabs = (initialTab, allTabs) => {
-    const [currentIndex, setCurrentIndex] = useState(initialTab);
-
-    if(!allTabs || !Array.isArray(allTabs)) {
-        return;
-    }
-
-    return {
-        currentItem: allTabs[currentIndex],
-        changeItem: setCurrentIndex
-    }
-}
+import React, {useEffect, useState} from "react";
 
 const App = () => {
-    const {currentItem,changeItem} = useTabs(0, content);
+
     return (
         <div className="App">
-            {content.map((section, index) =>  (
-                <button onClick={() => changeItem(index)} key={index}>{section.tab}</button>
-            ))}
-            <div>{currentItem.content}</div>
+            <div>Hi</div>
         </div>
     )
 }
